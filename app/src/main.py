216229ce37e4611568814a2a9bd03ec831e8920e
Stefan Stancu <stefan.stancu15@gmail.com>
2018-01-16 15:10:32 -0500

    Set backend to debug mode

--- app/src/main.py
+++ app/src/main.py
@@ -1,8 +1,9 @@
 from os import sys
-from flask import Flask, request
+
 from database import db_session
-from models import Food
+from flask import Flask, request
 from flask_cors import CORS
+from models import Food
 
 app = Flask(__name__)
 CORS(app)
@@ -11,23 +12,25 @@ CORS(app)
 def hello_world():
     return 'What\'s good?'
 
+
 # Endpoint to save new food
-@app.route('/api/savefood', methods=['GET', 'POST','OPTIONS'])
+@app.route('/api/savefood', methods=['GET', 'POST', 'OPTIONS'])
 def save_food():
     if request.method == 'POST':
         try:
             data = request.get_json()
-            print(data, file=sys.stderr)
+            app.logger.info('Received data: ' + str(data))
             food = Food(**data)
             db_session.add(food)
             db_session.commit()
-        except Exception:
+        except Exception as ex:
+            app.logger.info(ex)
             return 'Error saving food'
         return 'Food Saved'
     else:
         return 'GET'
 
+
 @app.teardown_appcontext
 def shutdown_session(exception=None):
     db_session.remove()
-
